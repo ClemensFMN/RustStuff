209b95b8ac90eeabc604bb5cd0b337caa6a3f57a
Clemens Novak <clemens@familie-novak.net>
2021-03-01 20:17:03 +0100

    error handling

--- basics/array.rs
+++ basics/array.rs
@@ -2,13 +2,26 @@
 
 
 fn main() {
-   println!("Hello World");
-   let buf1 = [3; 5];
+    println!("Hello World");
+    let buf1 = [3; 5];
 
-   println!("Buffer Length: {}", buf1.len());
+    println!("Buffer Length: {}", buf1.len());
 
-   for x in &buf1 {
-       println!("{}", x);
-   }
+    for x in &buf1 {
+        println!("{}", x);
+    }
+    // this does not work as the buf is R/O
+    // buf1[0] = 0;
    
+    println!("***********************");
+
+    let mut buf2 = [3; 5];
+
+    for x in &buf2 {
+        println!("{}", x);
+    }
+    buf2[0] = 0;
+    for x in &buf2 {
+        println!("{}", x);
+    }
 }
